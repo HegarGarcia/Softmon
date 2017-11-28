45867a576a694d4babdcc9856f1969c54ea77bb7
theoliver264 <31642690+theoliver264@users.noreply.github.com>
2017-11-27 22:38:57 -0600

    Add fire class

---
+++ Class_Pokemon_Fire.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Softmon
+{
+    class PokemonFire : Pokemon
+    {
+        public float Attacking(Pokemon defender)
+        {
+            float levelDamage = ((2 * this.Level) / 5) + 2;
+            float modifier = Effectiveness(defender.Type);
+            float attackVsDefence = this.Attack / defender.Defence;
+            return ((levelDamage * attackVsDefence) / 50 + 2) * modifier;
+        }
+
+        private float Effectiveness(string dType) => 1f;
+    }
+}
