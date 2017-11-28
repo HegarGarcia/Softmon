06a085802e4ee78948df8fbfc3bc5daacb527eeb
HegarGarcia <hegargarcia@gmail.com>
2017-11-27 22:17:53 -0600

    Adding Class Pokemon Normal

--- Class_Pokemon_Normal.cs
+++ Class_Pokemon_Normal.cs
@@ -9,6 +9,14 @@ namespace Softmon
 {
     public class PokemonNormal : Pokemon
     {
+        public float Attacking(Pokemon defender)
+        {
+            float levelDamage = ((2 * this.Level) / 5) + 2;
+            float modifier = Effectiveness(defender.Type);
+            float attackVsDefence = this.Attack / defender.Defence;
+            return ((levelDamage * attackVsDefence) / 50 + 2) * modifier;
+        }
 
+        private float Effectiveness(string dType) => 1f;
     }
 }
