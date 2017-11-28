4c4e8dcc761abb84771e79a8b6bf0f6e6b966011
HegarGarcia <hegargarcia@gmail.com>
2017-11-28 10:15:18 -0600

    Adding Trainer Class

---
+++ Class_Trainer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Softmon
+{
+    [DataContract]
+    public class Trainer
+    {
+        [DataMember]
+        private string Name
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        private string City
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        private string Gender
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        private List<Pokemon> Pokemons = new List<Pokemon>();
+
+        public Trainer(string nombre)
+        {
+            this.Name = nombre;
+        }
+
+        
+    }
+}
