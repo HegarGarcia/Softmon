6aa6a5d37eadd895ecc4b9591174ebcdeda386ca
HegarGarcia <hegargarcia@gmail.com>
2017-11-28 21:34:54 -0600

    Major refactory of classes

---
+++ Trainer Registration.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Softmon
+{
+    partial class Trainer_Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Nombre = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.createTrainerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Location = new System.Drawing.Point(12, 9);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(44, 13);
+            this.Nombre.TabIndex = 0;
+            this.Nombre.Text = "Nombre";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(12, 25);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(260, 20);
+            this.nameTextBox.TabIndex = 1;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.nameTextBox_TextChanged);
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(12, 69);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(260, 20);
+            this.cityTextBox.TabIndex = 3;
+            this.cityTextBox.TextChanged += new System.EventHandler(this.cityTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ciudad";
+            // 
+            // createTrainerButton
+            // 
+            this.createTrainerButton.Enabled = false;
+            this.createTrainerButton.Location = new System.Drawing.Point(174, 101);
+            this.createTrainerButton.Name = "createTrainerButton";
+            this.createTrainerButton.Size = new System.Drawing.Size(98, 23);
+            this.createTrainerButton.TabIndex = 4;
+            this.createTrainerButton.Text = "Crear Entrenador";
+            this.createTrainerButton.UseVisualStyleBackColor = true;
+            this.createTrainerButton.Click += new System.EventHandler(this.createTrainerButton_Click);
+            // 
+            // Trainer_Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 136);
+            this.Controls.Add(this.createTrainerButton);
+            this.Controls.Add(this.cityTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.Nombre);
+            this.Name = "Trainer_Registration";
+            this.Text = "Trainer Resgistrator";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Trainer_Registration_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox cityTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button createTrainerButton;
+    }
+}
---
+++ Trainer Registration.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Softmon
+{
+    public partial class Trainer_Registration : Form
+    {
+        public Trainer_Registration()
+        {
+            InitializeComponent();
+        }
+
+        public Trainer player
+        {
+            get;
+            set;
+        } = null;
+
+        private void createTrainerButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            string city = cityTextBox.Text;
+
+            player = new Trainer(name, city);
+            this.Close();
+        }
+
+        private void nameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            createTrainerButton.Enabled = !string.IsNullOrWhiteSpace(this.nameTextBox.Text) &&
+                !string.IsNullOrWhiteSpace(this.cityTextBox.Text);
+        }
+
+        private void cityTextBox_TextChanged(object sender, EventArgs e)
+        {
+            createTrainerButton.Enabled = !string.IsNullOrWhiteSpace(this.nameTextBox.Text) &&
+                !string.IsNullOrWhiteSpace(this.cityTextBox.Text);
+        }
+
+        private void Trainer_Registration_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(createTrainerButton.Enabled == true)
+            {
+                string name = nameTextBox.Text;
+                string city = cityTextBox.Text;
+
+                player = new Trainer(name, city);
+                this.Close();
+            }
+        }
+    }
+}
