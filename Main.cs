18ef5f11de43dc88b07db7312a474fc10ad08af2
HegarGarcia <hegargarcia@gmail.com>
2017-11-29 00:30:14 -0600

    Fragmenting methods

--- Main.cs
+++ Main.cs
@@ -53,66 +53,99 @@ namespace Softmon
             DataContractSerializer pokedexSerializer = new DataContractSerializer(typeof(List<Pokemon>));
             DataContractSerializer trainerSerializer = new DataContractSerializer(typeof(Trainer));
 
-            bool pokedexExists = File.Exists(pokedexFilePath);
-            bool trainerExists = File.Exists(trainerFilePath);
-
             try //Read file
             {
-                //Get Pokedex File
-                FileStream pokedexFile = new FileStream(pokedexFilePath, FileMode.Open);
-                FileStream trainerFile = new FileStream(trainerFilePath, FileMode.Open);
-
-                //Get data from file
-                Pokedex = (List<Pokemon>)pokedexSerializer.ReadObject(pokedexFile);
-                Player = (Trainer)trainerSerializer.ReadObject(trainerFile);
-
-                //Close file
-                pokedexFile.Close();
-                trainerFile.Close();
+                LoadPokedex(pokedexSerializer);
+                LoadTrainer(trainerSerializer);
             }
             catch //Create and fill File
             {
                 XmlWriterSettings settings = new XmlWriterSettings { Indent = true };
+                bool pokedexExists = File.Exists(pokedexFilePath);
+                bool trainerExists = File.Exists(trainerFilePath);
 
                 if (!pokedexExists) //Create Pokedex File and Set data to file
-                {
-                    //Adding starting pokemons...
-                    Pokemon bulbasaur = new PokemonGrass() { Name = "Bulbasaur", Health = 45, Attack = 49, Defense = 49, SpAttack = 65, Id = 1, };
-                    Pokemon charmander = new PokemonFire() { Name = "Charmander", Health = 39, Attack = 52, Defense = 43, SpAttack = 60, Id = 2 };
-                    Pokemon squirtle = new PokemonWater() { Name = "Squirtle", Health = 44, Attack = 48, Defense = 65, SpAttack = 50, Id = 3 };
-                    Pokemon pidgey = new PokemonFlying() { Name = "Pidgey", Health = 40, Attack = 45, Defense = 40, SpAttack = 35, Id = 4 };
-                    Pokemon rattata = new PokemonNormal() { Name = "Rattata", Health = 30, Attack = 56, Defense = 35, SpAttack = 25, Id = 5 };
-                    Pokemon spearow = new PokemonFlying() { Name = "Spearow", Health = 40, Attack = 60, Defense = 30, SpAttack = 31, Id = 6 };
-
-                    Pokedex.Add(bulbasaur);
-                    Pokedex.Add(charmander);
-                    Pokedex.Add(squirtle);
-                    Pokedex.Add(pidgey);
-                    Pokedex.Add(rattata);
-                    Pokedex.Add(spearow);
-
-                    XmlWriter pokedexFile = XmlWriter.Create(pokedexFilePath, settings);
-                    pokedexSerializer.WriteObject(pokedexFile, Pokedex);
-                    pokedexFile.Close();
-                }
+                    CreatePokedex(pokedexSerializer, settings);
 
                 if (!trainerExists) //Create Trainer File and Set data to file
-                {
-                    var form = new Trainer_Registration();
+                    CreateTrainer(trainerSerializer, settings);
 
-                    form.ShowDialog();
-
-                    Player = (form.player == null ? new Trainer("Ash", "Pueblo Paleta") : form.player);
-                    
-                    XmlWriter trainerFile = XmlWriter.Create(trainerFilePath, settings);
-                    trainerSerializer.WriteObject(trainerFile, Player);
-                    trainerFile.Close();
-                }
                 this.Close();
-
                 Application.Restart();
             }
-        }   
-        
+        }
+
+        public void LoadPokedex(DataContractSerializer serializer)
+        {
+            //Get Pokedex File
+            FileStream pokedexFile = new FileStream(pokedexFilePath, FileMode.Open);
+
+            //Get data from file
+            Pokedex = (List<Pokemon>)serializer.ReadObject(pokedexFile);
+
+            //Close file
+            pokedexFile.Close();
+        }
+
+        public void LoadTrainer(DataContractSerializer serializer)
+        {
+            //Get Pokedex File
+            FileStream trainerFile = new FileStream(trainerFilePath, FileMode.Open);
+
+            //Get data from file
+            Player = (Trainer)serializer.ReadObject(trainerFile);
+
+            //Close file
+            trainerFile.Close();
+        }
+
+        private void CreatePokedex(DataContractSerializer serializer, XmlWriterSettings settings)
+        {
+            //Adding starting pokemons...
+            Pokemon bulbasaur = new PokemonGrass() { Name = "Bulbasaur", Health = 45,
+                Attack = 49, Defense = 49, SpAttack = 65, Id = 1, };
+
+            Pokemon charmander = new PokemonFire() { Name = "Charmander", Health = 39,
+                Attack = 52, Defense = 43, SpAttack = 60, Id = 2 };
+
+            Pokemon squirtle = new PokemonWater() { Name = "Squirtle", Health = 44,
+                Attack = 48, Defense = 65, SpAttack = 50, Id = 3 };
+
+            Pokemon pidgey = new PokemonFlying() { Name = "Pidgey", Health = 40,
+                Attack = 45, Defense = 40, SpAttack = 35, Id = 4 };
+
+            Pokemon rattata = new PokemonNormal() { Name = "Rattata", Health = 30,
+                Attack = 56, Defense = 35, SpAttack = 25, Id = 5 };
+
+            Pokemon spearow = new PokemonFlying() { Name = "Spearow", Health = 40,
+                Attack = 60, Defense = 30, SpAttack = 31, Id = 6 };
+
+            Pokedex.Add(bulbasaur);
+            Pokedex.Add(charmander);
+            Pokedex.Add(squirtle);
+            Pokedex.Add(pidgey);
+            Pokedex.Add(rattata);
+            Pokedex.Add(spearow);
+
+
+            XmlWriter pokedexFile = XmlWriter.Create(pokedexFilePath, settings);
+            serializer.WriteObject(pokedexFile, Pokedex);
+            pokedexFile.Close();
+        }
+
+        private void CreateTrainer(DataContractSerializer serializer, XmlWriterSettings settings)
+        {
+            var form = new Trainer_Registration();
+            form.ShowDialog();
+
+            Player = (form.player == null ? new Trainer("Ash", "Pueblo Paleta") : form.player);
+
+            Player.ChosePokemon(Pokedex);
+
+            XmlWriter trainerFile = XmlWriter.Create(trainerFilePath, settings);
+            serializer.WriteObject(trainerFile, Player);
+            trainerFile.Close();
+        }
+
     }
 }
